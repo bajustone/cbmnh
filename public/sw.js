955b89c84a498a5ed67396c492f431489b709919
BAHATI Jutin <bajustone@gmail.com>
2021-04-08 12:16:37 +0200

    setting sw

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'cbmnh-v0';
+var urlsToCache = [
+  '/story_content/video_5kDlgJ50Sw0_22_48_458x336.mp4',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
